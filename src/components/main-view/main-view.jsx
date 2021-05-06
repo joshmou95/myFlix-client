0fefca81cdbad1714ab8b7805c69509a138d1365
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-06 17:19:08 -0600

    Added axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 // requirement for creating a component
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -11,13 +13,22 @@ export class MainView extends React.Component {
     super();
     // MainView state initialized with movies obj
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg' },
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg' },
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png' }
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount () {
+    // online database
+    axios.get('https://myflixdb2000.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie (newSelectedMovie) {
@@ -32,7 +43,7 @@ export class MainView extends React.Component {
     // display a clicked movies details
     // if (selectedMovie) return <MovieView movie={selectedMovie} />;
     // display message if there are no movies in main-view's state
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
     return (
       <div className="main-view">
       {selectedMovie
