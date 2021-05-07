e170340892a5d3f0d7720273606bb0d5b46d4ba0
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-07 15:06:43 -0600

    Added proptypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export function LoginView (props) {
-  /* call useState() method with an empty string, the initial value of the login variable
-     this method returns an arry that you destructure */
+  /* call useState() method with an empty string, the initial value of the login variable. This method returns an arry that you destructure */
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -15,16 +15,27 @@ export function LoginView (props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <div>
+    <h3>Login to MyFlix here!</h3>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
+      <a href='../registration-view/registration-view'>Click here to Register</a>
+    </div>
   );
 }
+LoginView.propTypes = {
+  register: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }),
+  onLoggedIn: PropTypes.func.isRequired
+};
