425961f62d1ac049c011fb66b3fd9ed1c6fb2e08
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-09 23:42:38 -0500

    Added bootstrap react components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,9 @@
 // requirement for creating a component
 import React from 'react';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './main-view.scss';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
@@ -68,16 +71,24 @@ export class MainView extends React.Component {
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
+    /* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all movies will be returned */
     return (
-      <div className="main-view">
-      {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all movies will be returned */}
-      {selectedMovie
-        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-        : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }}/>
-        ))
-      }
-    </div>
+      <Row className="main-view justify-content-md-center">
+        {selectedMovie
+          ? (
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            )
+          : (
+              movies.map(movie => (
+                <Col md={3}>
+                  <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                </Col>
+              ))
+            )
+        }
+      </Row>
     );
   }
 }
