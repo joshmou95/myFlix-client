33614608e8dadf10e14031fb8bc19f5c2dc9b837
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-14 18:53:17 -0600

    import axios

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
@@ -15,8 +16,21 @@ export function RegistrationView (props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    props.onRegister(username);
+    axios.post('https://myflixdb2000.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        // '_self' will open in the current tab
+        window.open('/', '_self');
+      })
+      .catch(e => {
+        console.log('error registering the user');
+      });
   };
 
   return (
@@ -27,15 +41,15 @@ export function RegistrationView (props) {
             <h3>Register for MyFlix</h3>
             <Form.Group controlId="formUsername">
               <Form.Label>Username:</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} required />
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
+              <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} required />
             </Form.Group>
             <Form.Group controlId="FormEmail">
               <Form.Label>Email:</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} required />
             </Form.Group>
             <Form.Group controlId="formBirthday">
               <Form.Label>Birthday:</Form.Label>
@@ -53,6 +67,5 @@ RegistrationView.propTypes = {
     username: PropTypes.string.isRequired,
     password: PropTypes.string.isRequired,
     birthday: PropTypes.number
-  }),
-  onRegister: PropTypes.func.isRequired
+  })
 };
