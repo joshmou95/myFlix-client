93c71d5a2f31ff4bcff9237803f92cfc4427c90e
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-14 18:51:12 -0600

    import router-dom

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,18 @@
 // requirement for creating a component
 import React from 'react';
 import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import './main-view.scss';
 
-// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { RegistrationView } from '../registration-view/registration-view';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // exposes MainView class component to use by other components
 export class MainView extends React.Component {
@@ -23,22 +25,9 @@ export class MainView extends React.Component {
     // selectedMovie and user are set to null
     this.state = {
       movies: [],
-      selectedMovie: null,
+      // selectedMovie: null,
       user: null
     };
-  }
-
-  getMovies (token) {
-    axios.get('https://myflixdb2000.herokuapp.com/movies', {
-      // pass bearer auth in the header of the http reqest
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-      // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      });
   }
 
   // execute after the component is added to the DOM
@@ -52,6 +41,22 @@ export class MainView extends React.Component {
       // call getMovies method GET request to movies endpoint
       this.getMovies(accessToken);
     }
+  }
+
+  getMovies (token) {
+    axios.get('https://myflixdb2000.herokuapp.com/movies', {
+      // pass bearer auth in the header of the http reqest
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+      // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
@@ -92,36 +97,54 @@ export class MainView extends React.Component {
 
   // returns visual representation of the component
   render () {
-    const { movies, selectedMovie, user, register } = this.state;
+    const { movies, user } = this.state;
 
-    if (!register) return <RegistrationView onRegister={register => this.onRegister(register)}/>;
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    // Before the movies have been loaded
-    if (movies.length === 0) return <div className="main-view" />;
-
-    /* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all movies will be returned */
     return (
-      <div>
-        <button onClick={() => { this.onLoggedOut() }}>Logout</button>
+      <Router>
+        {/* <button onClick={() => { this.onLoggedOut() }}>Logout</button> */}
         <Row className="main-view justify-content-md-center">
-          {selectedMovie
-            ? (
-                <Col md={8}>
-                  <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                </Col>
-              )
-            : (
-                movies.map(movie => (
-                  <Col md={3}>
-                    <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                  </Col>
-                ))
-              )
-          }
+          <Route exact path="/" render={() => {
+            if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+            if (movies.length === 0) return <div className="main-view" />;
+            return movies.map(m => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
+              </Col>
+            ));
+          }} />
+          <Route path="/register" render={() => {
+            if (user) return <Redirect to="/" />;
+            return <Col>
+              <RegistrationView />
+            </Col>;
+          }} />
+          <Route path="/movies/:movieId" render={({ match, history }) => {
+            if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+            </Col>;
+          }} />
+          <Route path="/genres/:name" render={ ({ match }) => {
+            if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre }onBackClick={() => history.goBack()} />
+            </Col>;
+          }} />
+          <Route path="/director/:name" render={({ match, history }) => {
+            if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+            // if there are no movies return main-view
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              {/* find directors name from the database */}
+              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+            </Col>;
+          }} />
         </Row>
-      </div>
+      </Router>
     );
   }
 }
