6a47952548fe84ca434312a0bcc386474ef55f6a
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-16 13:08:01 -0600

    removed proptypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,9 +4,9 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import PropTypes from 'prop-types';
-import Row from 'react-bootstrap/Row';
 
 import { Link } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
 
 export function LoginView (props) {
   /* call useState() method with an empty string, the initial value of the login variable. This method returns an arry that you destructure */
@@ -14,9 +14,8 @@ export function LoginView (props) {
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
-    // method prevents the default refresh/change of the page from handleSubmit method
     e.preventDefault();
-    /* Send a request to the server for authentication then call props.onLoggedIn(username) */
+    /* Send a request to the server for authentication */
     axios.post('https://myflixdb2000.herokuapp.com/login', {
       Username: username,
       Password: password
@@ -31,43 +30,37 @@ export function LoginView (props) {
   };
 
   return (
-    <Row>
+    <Col xs={8} lg={6}>
       <Form>
         <br></br>
-        <div xs={8} lg={6}>
           <h3>Login to MyFlix</h3>
-          <Form.Group controlId="formUsername">
+          <Form.Group controlId="formBasicUsername">
             <Form.Label>Username:</Form.Label>
             <Form.Control
-            type="text"
-            value={username}
+            type="text" value={username}
             onChange={e => setUsername(e.target.value)}
-            placeholder="Enter username"
-            required />
+            placeholder="Enter username" />
             </Form.Group>
-            <Form.Group controlId="formPassword">
+            <Form.Group controlId="formBasicPassword">
             <Form.Label>Password:</Form.Label>
             <Form.Control
-              type="password"
-              value={password}
+              type="password" value={password}
               onChange={e => setPassword(e.target.value)}
-              placeholder="Enter Password"
-              required />
+              placeholder="Enter Password" />
           </Form.Group>
             <Button variant="primary" type="submit" onClick={handleSubmit}>Submit </Button>
             <hr />
             <Link to="/register">
               <Button variant="info" type="button"> Register</Button>
             </Link>
-        </div>
       </Form>
-    </Row>
+    </Col>
   );
 }
 LoginView.propTypes = {
-  register: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-  }),
+  // register: PropTypes.shape({
+  //   Username: PropTypes.string.isRequired,
+  //   Password: PropTypes.string.isRequired
+  // }),
   onLoggedIn: PropTypes.func.isRequired
 };
