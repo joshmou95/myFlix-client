0767d915e33cd0bee5d9754632df3111a277ad5a
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-23 16:54:29 -0600

    edited proptypes

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -7,42 +7,38 @@ import { Link } from 'react-router-dom';
 
 export class DirectorView extends React.Component {
   render () {
-    const { movie, onBackClick } = this.props;
+    const { director, onBackClick } = this.props;
 
     return (
+      <div>
       <Card className="director-view">
         <Card.Body>
-          <Card.Title className="director-name">
-            <span className="label">Name: </span>
-            <span className="value">{movie.Director.Name}</span>
-          </Card.Title>
+            <Card.Title>
+            <span className="value">{director.Name}</span>
+            </Card.Title>
           <Card.Text className="director-birthday">
             <span>Born in: </span>
-            <span className="value">{movie.Director.Birth}</span>
+            <span className="value">{director.Birth}</span>
           </Card.Text>
           <Card.Text className="director-bio">
             <span>Biography: </span>
-            <span className="value">{movie.Director.Bio}</span>
+            <span className="value">{director.Bio}</span>
           </Card.Text>
-          <Button onClick={() => { onBackClick(null); }}>Back</Button>
+          <Button variant="secondary" onClick={() => { onBackClick(null); }}>Back</Button>
         </Card.Body>
       </Card>
+      </div>
+      
     );
   }
 }
+
 DirectorView.propTypes = {
-  movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string,
-      Death: PropTypes.string
-    }),
-    Actors: PropTypes.array
-  }).isRequired,
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+    Death: PropTypes.string,
+  }),
   onBackClick: PropTypes.func.isRequired
 };
