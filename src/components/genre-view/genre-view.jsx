202bc87c96f8dc410434aeeeef5bb6b2e9b34e02
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-23 16:54:39 -0600

    Created genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -3,47 +3,51 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import Container from 'react-bootstrap/Container';
+
+import { MovieCard } from '../movie-card/movie-card'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 
 export class GenreView extends React.Component {
   render () {
-    const { movie, movies, onBackClick } = this.props;
+    const { genre, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Card className="genre-view">
-          <Card.Body>
-            <Card.Title className="genre-name">
-              <span className="value">{movie.Genre.Name}</span>
+      <div>
+      <Card className="genre-view">
+        <Card.Body>
+            <Card.Title>
+            <span className="value">{genre.Name}</span>
             </Card.Title>
-            <Card.Text className="director-birthday">
-              <span>Description: </span>
-              <span className="value">{movie.Genre.Description}</span>
-            </Card.Text>
-            <Button onClick={() => { onBackClick(null); }}>Back</Button>
-          </Card.Body>
-        </Card>
-        <div>
-          <h4>Some {movie.Genre.Name} movies</h4>
-        </div>
-        <div>
-        </div>
-      </Container>
+          <Card.Text className="genre-discription">
+            <span className="value">{genre.Description}</span>
+          </Card.Text>
+          <Button variant="secondary" onClick={() => { onBackClick(null); }}>Back</Button>
+        </Card.Body>
+      </Card>
+      {/* <Container>
+        <Row>
+          {
+            movies.map( (m, i) => (
+              <Col xs={4} lg={3} key={i} className="p-2">
+                <MovieCard key={m._id} movie={m} />
+              </Col>
+            ))
+          }
+        </Row>
+      </Container> */}
+      </div> 
     );
   }
 }
+
 GenreView.propTypes = {
-  movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Actors: PropTypes.array
-  }).isRequired,
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }),
   onBackClick: PropTypes.func.isRequired
 };
+
