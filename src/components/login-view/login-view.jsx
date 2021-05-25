cc38dbe5b4d58988c388cd4f03c7133f3c2f2e86
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-24 22:10:55 -0600

    Added validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -17,7 +17,6 @@ export function LoginView (props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
     // axios.post('http://localhost:8080/login', {
     axios.post('https://myflixdb2000.herokuapp.com/login', null, {
@@ -27,7 +26,6 @@ export function LoginView (props) {
     }})
     .then(response => {
       const data = response.data;
-      console.log(data);
       props.onLoggedIn(data);
     })
     .catch(e => {
@@ -36,35 +34,37 @@ export function LoginView (props) {
   };
 
   return (
-    <Col xs={8} lg={6}>
-      <Form>
-        <br></br>
-          <h3>Login to MyFlix</h3>
-          <Form.Group controlId="formUsername">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control
-            type="text"
-            value={username}
-            placeholder="Enter username"
-            onChange={e => setUsername(e.target.value)}
-            autoComplete="username" />
+      <Col xs={6} lg={6} className="justify-content-md-center">
+        <Form>
+          <br></br>
+            <h3>Login to MyFlix</h3>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+              type="text"
+              value={username}
+              placeholder="Enter username"
+              onChange={e => setUsername(e.target.value)}
+              autoComplete="username" 
+              required minLength="5" />
+              </Form.Group>
+              <Form.Group controlId="formPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                value={password}
+                type="password"
+                placeholder="Password"
+                onChange={e => setPassword(e.target.value)}
+                autoComplete="password" 
+                required />
             </Form.Group>
-            <Form.Group controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              value={password}
-              type="password"
-              placeholder="Password"
-              onChange={e => setPassword(e.target.value)}
-              autoComplete="password" />
-          </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit </Button>
-            <hr />
-            <Link to="/register">
-              <Button variant="info" type="button"> Register</Button>
-            </Link>
-      </Form>
-    </Col>
+              <Button variant="primary" type="submit" onClick={handleSubmit}>Submit </Button>
+              <hr />
+              <Link to="/register">
+                <Button variant="info" type="button"> Register</Button>
+              </Link>
+        </Form>
+        </Col>  
   );
 }
 LoginView.propTypes = {
