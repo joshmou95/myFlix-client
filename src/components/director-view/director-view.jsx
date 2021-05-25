6b88bbc44324d7fa371c0c9a50aa4e98cfc68e50
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-24 22:10:27 -0600

    Updated director-view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -4,29 +4,36 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { Link } from 'react-router-dom';
+import './director-view.scss'
+import { Container } from 'react-bootstrap';
+import { MovieCard } from '../movie-card/movie-card';
 
 export class DirectorView extends React.Component {
   render () {
-    const { director, onBackClick } = this.props;
+    const { movies, director, onBackClick } = this.props;
 
     return (
       <div>
-      <Card className="director-view">
-        <Card.Body>
-            <Card.Title>
-            <span className="value">{director.Name}</span>
-            </Card.Title>
-          <Card.Text className="director-birthday">
-            <span>Born in: </span>
-            <span className="value">{director.Birth}</span>
-          </Card.Text>
-          <Card.Text className="director-bio">
-            <span>Biography: </span>
-            <span className="value">{director.Bio}</span>
-          </Card.Text>
-          <Button variant="secondary" onClick={() => { onBackClick(null); }}>Back</Button>
-        </Card.Body>
-      </Card>
+        <Card className="director-view">
+          <Card.Body>
+              <Card.Title>
+              <span className="value">{director.Name}</span>
+              </Card.Title>
+            <Card.Text className="director-birthday">
+              <span>Born in: </span>
+              <span className="value">{director.Birth}</span>
+            </Card.Text>
+            <Card.Text className="director-bio">
+              <span>Biography: </span>
+              <span className="value">{director.Bio}</span>
+            </Card.Text>
+            <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
+          </Card.Body>
+        </Card>
+
+        <Container>
+          <h4 className="mt-4">Some {director.Name} movies</h4>
+        </Container>
       </div>
       
     );
