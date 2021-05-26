01531fa8d44155941d09d3bd7b2c49eabb348364
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-26 11:39:39 -0600

    updated margins

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -34,7 +34,7 @@ export class GenreView extends React.Component {
         {movies.map((m) => {
           if (m.Genre.Name === genre.Name) {
             return (
-              <div className="d-inline-flex align-items-start m-1" key={m._id}>
+              <div className="d-inline-flex align-items-start" key={m._id}>
                 <MovieCard movie={m} />
               </div>
             )
