4abaede2484330e43ce1b123079160ce1f64df2a
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-05 10:55:05 -0600

    Created Movies List to Filter Movies by title

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
+
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import { MovieCard } from '../movie-card/movie-card';
+
+
+// extract visibilityFilter into a prop called visibilityFilter
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+  }
+
+  if (!movies) return <div className="main-view"/>;
+
+  return <>
+    <Col md={12} className="m-3">
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+    </Col>
+    {filteredMovies.map(m => (
+      <Col md={3} key={m._id}>
+        <MovieCard movie={m} />
+      </Col>
+    ))}
+  </>;
+}
+
+// transforms the store into props that the MoviesList component will use
+export default connect(mapStateToProps)(MoviesList);
