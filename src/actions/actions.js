e88e5fca5c0e5adcf9f8cb381c5835c7d0e27908
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-06 13:45:57 -0600

    added SET_USER

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,10 @@
+// Action Types:
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
 
+
+// Actions:
 // initialize the movies property
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -9,4 +13,9 @@ export function setMovies(value) {
 // change the visibilityFilter property
 export function setFilter(value) {
   return { type: SET_FILTER, value };
+}
+
+export function setUser(value) {
+  console.log('SET_USER action triggered');
+  return { type: SET_USER, value };
 }
