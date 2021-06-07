88c712b1f3ad3d3016be5f201c512a757ccf78fe
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-06 19:26:46 -0600

    set state to empty array

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -22,7 +22,7 @@ function movies(state = [], action) {
   }
 }
 
-function user(state = null, action) {
+function user(state = [], action) {
   switch (action.type) {
     case SET_USER:
       console.log('SET_USER reducer reached');
