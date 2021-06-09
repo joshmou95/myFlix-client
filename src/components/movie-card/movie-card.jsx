83f09a92b308189dc5268e459c327d61eb52287a
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-08 21:14:20 -0600

    passed props

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,6 +7,10 @@ import './movie-card.scss';
 import { Link } from 'react-router-dom';
 
 export class MovieCard extends React.Component {
+  constructor (props) {
+    super (props)
+  }
+  
   render () {
     const { movie } = this.props;
 
