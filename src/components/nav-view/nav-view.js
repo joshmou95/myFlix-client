a12d3e8f356b4547c909b1b3103b1460d09fa5f6
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-09 10:03:45 -0600

    update profile url

--- src/components/nav-view/nav-view.js
+++ src/components/nav-view/nav-view.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 export class NavView extends React.Component {
+  constructor (props) {
+    super (props)
+  }
+  
   onLoggedOut () {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -18,7 +22,7 @@ export class NavView extends React.Component {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="ml-auto">    
-        <Nav.Link href="/profile">Profile</Nav.Link>
+        <Nav.Link href={`/users/${this.props.user}`}>Profile</Nav.Link>
         <Nav.Link href="/" onClick={() => { this.onLoggedOut() }}>Log Out</Nav.Link>
       </Nav>
       </Navbar.Collapse>
