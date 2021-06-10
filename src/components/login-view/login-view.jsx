983aacc812c9b5027c65892a2c6d296b547038a4
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-09 21:37:56 -0600

    fixed login security issue

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -19,11 +19,10 @@ export function LoginView (props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post('https://myflixdb2000.herokuapp.com/login', null, {
-      params: {
-      Username: username,
-      Password: password
-    }})
+    axios.post('https://myflixdb2000.herokuapp.com/login', { 
+      username: username, 
+      password: password 
+  })
     .then(response => {
       const data = response.data;
       props.onLoggedIn(data);
