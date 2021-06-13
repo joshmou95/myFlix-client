a7b51a26387695502af989e006d10999d8cd0bad
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-13 10:49:27 -0600

    created update user action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,6 +2,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
+export const UPDATE_USER = "UPDATE_USER";
 
 // Actions:
 // initialize the movies property
@@ -18,4 +19,9 @@ export function setFilter(value) {
 export function setUser(value) {
   console.log('SET_USER action triggered');
   return { type: SET_USER, value };
+}
+
+export function updateUser(value) {
+  console.log('UPDATE_USER action triggered');
+  return { type: UPDATE_USER, value };
 }
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, UPDATE_USER } from '../actions/actions';
 
 // initilize state to empty string, pass in action
 function visibilityFilter(state = '', action) {
@@ -27,6 +27,9 @@ function user(state = [], action) {
   switch (action.type) {
     case SET_USER:
       console.log('SET_USER reducer reached');
+      return action.value
+    case UPDATE_USER:
+      console.log('UPDATE_USER reducer reached');
       return action.value
     default:
       return state;
